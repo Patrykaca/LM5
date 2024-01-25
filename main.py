434957712c482c5191f8dc1a13efa956178dab71
Patryk Kaca <patrykaca@gmail.com>
2024-01-25 02:26:13 +0100

    refactor

--- main.py
+++ main.py
@@ -15,122 +15,90 @@ class LL1:
 
         self.first_S = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(']
         self.first_P = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(']
-        self.follow_P = ['*', ':', '+', '-', '^', ')', '$']
         self.first_R = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         self.follow_R = ['*', ':', '+', '-', '^', '@', ';', ')', '$']
-        self.first_L = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        self.first_Lp = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@']
         self.first_W = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(']
-        self.follow_W = ['*', ':', '+', '-', '^', '@', ';', ')', '$']
 
         self.char = 0
 
         for i in expression:
             self.arithmetic_expression.append(i)
         print(self.arithmetic_expression)
-
-
-    def parse(self):
-        while self.arithmetic_expression[self.char] in self.first_R:
-            self.read_S()
-        return True
-
+    
+    def next_char(self):
+        self.char += 1
+        
+    def check_first(self, first):
+        return self.arithmetic_expression[self.char] in first
 
     def read_S(self):
-        if self.arithmetic_expression[self.char] in self.first_P:
-            self.read_P()
-            if self.arithmetic_expression[self.char] in self.O:
-                self.read_P()
-            if self.arithmetic_expression[self.char] in self.semicolon:
-                self.read_S()
-
-
-
-        if self.arithmetic_expression[self.char] in self.first_P:
-            self.read_P()
-            if self.arithmetic_expression[self.char] in self.O:
-                self.char = self.char + 1
-                self.read_S()
-            if self.arithmetic_expression[self.char] in self.semicolon:
-                # self.char = self.char + 1
-                self.read_S()
-            if self.arithmetic_expression[self.char] in self.eof:
-                return True
-        return False
-            # raise ValueError(f'Invalid self.character {arithmetic_expression[self.char]}. Expected one of {self.O} or {self.C} or {self.semicolon}')
-
-# Działa!!!!!!!!!!!
-    def read_P(self):
-        if self.arithmetic_expression[self.char] in self.first_P:
-            if self.arithmetic_expression[self.char] in self.C:
-                return self.read_R()
-            elif self.arithmetic_expression[self.char] in self.opening_bracket:
-                self.char = self.char + 1
-                if self.read_W():
-                    if self.arithmetic_expression[self.char] in self.closing_bracket:
-                        self.char = self.char + 1
+        if self.check_first(self.first_S):
+            if self.read_W():
+                if self.check_first(self.semicolon):
+                    self.next_char()
+                    if self.check_first(self.first_S):
+                        return self.read_S()
+                    elif self.check_first(self.eof):
                         return True
                     return False
+                return False
             return False
         return False
 
-# Działa!!!!!!!!!!!
+    def read_P(self):
+        if self.check_first(self.first_P):
+            if self.check_first(self.C):
+                return self.read_R()
+            elif self.check_first(self.opening_bracket):
+                self.next_char()
+                if self.read_W():
+                    if self.check_first(self.closing_bracket):
+                        self.next_char()
+                        return True
+                    return False
+                return False
+            return False
+        return False
+
     def read_W(self):
-        if self.arithmetic_expression[self.char] in self.first_W:
+        if self.check_first(self.first_W):
             if self.read_P():
-                if self.arithmetic_expression[self.char] in self.O:
-                    self.char = self.char + 1
+                if self.check_first(self.O):
+                    self.next_char()
                     return self.read_W()
                 return True
             return False
         return False
 
-
-
-# Działa!!!!!!!!!!!
     def read_R(self):
         dot_encountered = False  # Flag to indicate if a dot has been encountered
         digit_encountered_before_dot = False
 
         while True:
-            if self.arithmetic_expression[self.char] in self.first_R:
+            if self.check_first(self.first_R):
                 if not dot_encountered:
                     digit_encountered_before_dot = True
-                self.char = self.char + 1
-                continue # If the character is a digit, move to the next character
-            elif self.arithmetic_expression[self.char] in self.dot:
+                self.next_char()
+                continue  # If the character is a digit, move to the next character
+            elif self.check_first(self.dot):
                 if (dot_encountered or not digit_encountered_before_dot
-                    or self.arithmetic_expression[self.char + 1] not in self.first_R):
-                    return False # If dot already encountered or no digit before dot or no digit after dot, return False
-                dot_encountered = True # Set the flag if a dot is encountered for the first time
-                self.char = self.char + 1
-            elif self.arithmetic_expression[self.char] in self.follow_R:
+                        or self.arithmetic_expression[self.char + 1] not in self.first_R):
+                    return False  # If dot already encountered or no digit before dot or no digit after dot, return False
+                dot_encountered = True  # Set the flag if a dot is encountered for the first time
+                self.next_char()
+            elif self.check_first(self.follow_R):
                 return True
-            else: # If any other character is encountered, return False
+            else:  # If any other character is encountered, return False
                 return False
-
         # In case there is no '$' at the end of the string, which should not happen as per the requirements
         return False
 
-    def read_C(self):
-        if arithmetic_expression[self.char] in self.C:
-            self.char = self.char + 1
-            return True
-        self.char = self.char + 1
-        return False
-
-    def read_O(self):
-        if arithmetic_expression[self.char] in self.O:
-            self.char = self.char + 1
-            return True
-        self.char = self.char + 1
-        return False
 
 # arithmetic_expression = input('Enter: ')
-arithmetic_expression = '((2-6)-1.3)' + '$'
-ll1 = LL1(arithmetic_expression )
+arithmetic_expression = '(1.2*3)+5-(23.4+3)^3;8:8;3;' + '$'
+ll1 = LL1(arithmetic_expression)
 
-ret = ll1.read_P()
+ret = ll1.read_S()
 if ret:
     print('True')
 else:
